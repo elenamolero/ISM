f394687680466c356447d4970c6669ea8b6192c9
Serrano <alvaroserrano0407@gmail.com>
2024-12-09 17:40:11 +0100

    feat: initialize app with portrait orientation and add HomePage with navigation to LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'presentation/pages/Login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'petUco',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 183, 58, 58)),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body:  Center(
-          child: Text('Bienvenido',
-            style: TextStyle(fontSize: 54)
-          
+      home: const HomePage(), // Página inicial será HomePage.
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF1292F2),
+              Color(0xFF5AB8FF),
+              Color(0xFF69CECE),
+            ],
+            stops: [0.1, 0.551, 1.0],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Bienvenido',
+                style: TextStyle(fontSize: 54),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(), // Navega al LoginPage.
+                    ),
+                  );
+                },
+                child: const Text('Ir al Login'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-
