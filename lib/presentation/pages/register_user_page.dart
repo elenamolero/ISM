5f955da0325a1a608f8003eb5337a7c722f0b20c
Joaquín <i12samoj@uco.es>
2024-12-11 19:12:45 +0100

    feat: dropdown list setted

--- lib/presentation/pages/register_user_page.dart
+++ lib/presentation/pages/register_user_page.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 
 final outlineInputBorder = OutlineInputBorder(
   borderRadius: BorderRadius.circular(15.0),
-  borderSide: const BorderSide(color: Colors.blue, width: 2.0),
-);
-
-final theme = ThemeData(
-  colorSchemeSeed: const Color.fromARGB(255, 0, 140, 255)
+  borderSide: BorderSide.none,
 );
 
 void main() => runApp(const RegisterUserPage());
 
-class RegisterUserPage extends StatelessWidget {
+class RegisterUserPage extends StatefulWidget {
   const RegisterUserPage({super.key});
+
+  @override
+  State<RegisterUserPage> createState() => _RegisterUserPageState();
+}
+
+class _RegisterUserPageState extends State<RegisterUserPage> {
+  bool _showVet = false;
+  String? _selectedValue;
 
   @override
   Widget build(BuildContext context) {
@@ -20,28 +24,45 @@ class RegisterUserPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Registro',
       home: Scaffold(
-        backgroundColor: theme.primaryColor,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           title: const Text('Registro'),
           centerTitle: true,
         ),
-        body: const Column(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: UserInputField(
-                  text: Text('Introduce el correo'),
-                  icon: Icons.email_outlined),
+            const Padding(padding: EdgeInsets.all(20.0)),
+            const UserInputField(
+                text: Text('Introduce el correo'), icon: Icons.email_outlined),
+            const UserInputField(
+                text: Text('Introduce la contraseña'), icon: Icons.password),
+            const UserInputField(
+                text: Text('Repite la contraseña'), icon: Icons.password),
+            const UserInputField(
+                text: Text('Introduce tu teléfono'), icon: Icons.phone),
+            UserElectionDropdownMenu(
+              onChanged: (value) {
+                setState(() {
+                  _selectedValue = value;
+                  _showVet = _selectedValue == 'Veterinario';
+                });
+              },
             ),
-            Center(
-              child: UserInputField(
-                  text: Text('Introduce la contraseña'), icon: Icons.password),
+            Visibility(
+              visible: _showVet,
+              child: const UserInputField(
+                  text: Text('Introduce el nombre del negocio'),
+                  icon: Icons.business),
             ),
-            Center(
-              child: UserInputField(
-                  text: Text('Repite la contraseña'), icon: Icons.password),
+            Visibility(
+              visible: _showVet,
+              child: const UserInputField(
+                  text: Text('Introduce el CIF del negocio'),
+                  icon: Icons.document_scanner_outlined),
             ),
-            Center(child: UserElectionDropdownMenu())
+            const Spacer(),
+            const RegisterButton(),
           ],
         ),
       ),
@@ -49,28 +70,52 @@ class RegisterUserPage extends StatelessWidget {
   }
 }
 
-class UserElectionDropdownMenu extends StatelessWidget {
-  const UserElectionDropdownMenu({
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 5),
-      child: DropdownButtonFormField(
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: theme.secondaryHeaderColor,
-          enabledBorder: outlineInputBorder,
-          focusedBorder: outlineInputBorder,
-        ),
-        items: ["Veterinario", "Dueño de un animal"].map((name) {
-          return DropdownMenuItem(value: name, child: Text(name));
-        }).toList(),
-        onChanged: (value) {},
-      ),
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+          style: TextButton.styleFrom(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+              side: const BorderSide(color: Colors.white, width: 2)),
+          onPressed: () {},
+          child: const Text('Registrarse')),
     );
+  }
+}
+
+class UserElectionDropdownMenu extends StatelessWidget {
+  final ValueChanged<String?> onChanged;
+
+  const UserElectionDropdownMenu({
+    super.key,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        child: DropdownButtonFormField<String>(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            enabledBorder: outlineInputBorder,
+            focusedBorder: outlineInputBorder,
+          ),
+          items: ["Veterinario", "Dueño de un animal"].map((name) {
+            return DropdownMenuItem(value: name, child: Text(name));
+          }).toList(),
+          onChanged: onChanged,
+        ));
   }
 }
 
@@ -83,11 +128,11 @@ class UserInputField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
       child: TextField(
         decoration: InputDecoration(
             filled: true,
-            fillColor: theme.secondaryHeaderColor,
+            fillColor: Colors.white,
             enabledBorder: outlineInputBorder,
             focusedBorder: outlineInputBorder,
             hintText: text.data,
