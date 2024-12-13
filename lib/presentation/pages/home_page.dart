859475912c267e0ed0ef348b50e057c0605407ba
laFlorNegra <1230112@isep.ipp.pt>
2024-12-13 18:53:29 +0100

    feat: new pet buttom

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -60,30 +60,48 @@ class HomeUserPage extends StatelessWidget {
     return ListView.builder(
       shrinkWrap: true,
       padding: const EdgeInsets.all(16.0),
-      itemCount: pets.length,
+      itemCount: pets.length + 1, // Incrementa el itemCount para incluir el botón
       itemBuilder: (context, index) {
-        final pet = pets[index];
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 8.0),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          child: ListTile(
-            leading: const Icon(Icons.pets, color: Colors.blue, size: 40),
-            title: Text(
-              pet.name,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        if (index == 0) {
+          // Botón "New pet"
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            child: ListTile(
+              leading: const Icon(Icons.add, color: Colors.blue, size: 40),
+              title: const Text(
+                'New pet',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                // Manejo de tap en el botón "New pet"
+              },
             ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Name: ${pet.name}'),
-                Text('Owner: ${pet.ownerEmail}'),
-              ],
+          );
+        } else {
+          final pet = pets[index - 1]; // Ajusta el índice para las mascotas
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            child: ListTile(
+              leading: const Icon(Icons.pets, color: Colors.blue, size: 40),
+              title: Text(
+                pet.name,
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Name: ${pet.name}'),
+                  Text('Owner: ${pet.ownerEmail}'),
+                ],
+              ),
+              onTap: () {
+                // Manejo de tap en la tarjeta
+              },
             ),
-            onTap: () {
-              // Manejo de tap en la tarjeta
-            },
-          ),
-        );
+          );
+        }
       },
     );
   }
