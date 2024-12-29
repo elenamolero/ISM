17e5d72c7e500fda62109d5e41937e884d34ae93
laFlorNegra <1230112@isep.ipp.pt>
2024-12-29 13:34:40 +0100

    fix: use interfaces

--- lib/domain/usecases/impl/register_user_use_case.dart
+++ lib/domain/usecases/impl/register_user_use_case.dart
@@ -1,18 +1,19 @@
+import 'package:petuco/data/repository/user_repository_interface.dart';
 import 'package:petuco/domain/usecases/register_user_use_case_interface.dart';
 import '../../../data/repository/impl/user_repository.dart';
 import '../../entities/user.entity.dart';
 
 class RegisterUserInfoUseCase extends RegisterUserUseCaseInterface {
 
-  UserRepository userRepository;
+  UserRepositoryInterface userRepositoryInterface;
 
-  RegisterUserInfoUseCase({required this.userRepository});
+  RegisterUserInfoUseCase({required this.userRepositoryInterface});
   
   @override
   Future<void> registerUserInfo(User user) async {
     try {
       print('RegisterUserInfoUseCase: Starting registration for user: ${user.name}');
-      await userRepository.registerUserInfo(user);
+      await userRepositoryInterface.registerUserInfo(user);
       print('RegisterUserInfoUseCase: Registration successful for user: ${user.name}');
     } catch (e) {
       print('RegisterUserInfoUseCase: Registration failed for user: ${user.name}, Error: $e');
