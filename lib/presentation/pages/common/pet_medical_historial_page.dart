8f2a89501cf1246b29ddcc3dc9a9933d4dbdf71b
laFlorNegra <1230112@isep.ipp.pt>
2024-12-29 10:52:03 +0100

    fix: bettter code practices

--- lib/presentation/pages/common/pet_medical_historial_page.dart
+++ lib/presentation/pages/common/pet_medical_historial_page.dart
@@ -8,13 +8,15 @@ import 'package:petuco/di/dependency_injection.dart';
 import 'package:petuco/domain/entities/healthTest.entity.dart';
 import 'package:petuco/domain/usecases/impl/get_health_tests_use_case.dart';
 import 'package:petuco/presentation/blocs/healthTests/get_health_tests_bloc.dart';
+import 'package:petuco/presentation/widgets/custom_cards_widget.dart';
+import 'package:petuco/presentation/widgets/custom_text_widget.dart';
 import 'package:petuco/presentation/widgets/footer_widget.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 class PetMedicalHistorialPage extends StatefulWidget {
   static const String route = 'petHistory';
   final int petId;
-
   final String petName;
 
   const PetMedicalHistorialPage({Key? key, required this.petId, required this.petName}) : super(key: key);
@@ -25,20 +27,10 @@ class PetMedicalHistorialPage extends StatefulWidget {
 
 class _PetMedicalHistorialPageState extends State<PetMedicalHistorialPage> {
   int? _selectedIndex;
-  Future<String?>? _roleFuture;
 
   @override
   void initState() {
     super.initState();
-    _roleFuture = _getUserRole();
-  }
-
-  Future<String?> _getUserRole() async {
-    final user = Supabase.instance.client.auth.currentUser;
-    if (user != null) {
-      return user.userMetadata!['role'] as String?;
-    }
-    return null;
   }
 
   @override
@@ -46,91 +38,79 @@ class _PetMedicalHistorialPageState extends State<PetMedicalHistorialPage> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     String petname = widget.petName;
+    String? role = Supabase.instance.client.auth.currentUser?.userMetadata!['role'] as String?;
 
-    return FutureBuilder<String?>(
-      future: _roleFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else {
-          String? role = snapshot.data;
-          print('Role: $role');
-          return BlocProvider(
-            create: (context) => HealthTestBloc(
-              getHealthTestsUseCase: appInjector.get<GetHealthTestsUseCase>(),
-            )..add(FetchHealthTests(petId: widget.petId)),
-            child: Scaffold(
-              body: Stack(
-                children: [
-                  BackGround(title: 'History',isUserLoggedIn: true, page: PetInfoPage(petId: widget.petId)),
-                  Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(top: screenHeight * 0.13),
+    return BlocProvider(
+      create: (context) => HealthTestBloc(
+        getHealthTestsUseCase: appInjector.get<GetHealthTestsUseCase>(),
+      )..add(FetchHealthTests(petId: widget.petId)),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            BackGround(title: 'History', isUserLoggedIn: true, page: PetInfoPage(petId: widget.petId)),
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: screenHeight * 0.13),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Opacity(
+                    opacity: 0.69,
+                    child: Text(
+                      "Informes médicos de $petname",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white.withOpacity(0.53),
+                        fontWeight: FontWeight.bold,
+                        fontFamily: const String.fromEnvironment('InriaSans'),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 15),
-                        child: Opacity(
-                          opacity: 0.69,
-                          child: Text(
-                            "Informes médicos de $petname",
-                            style: TextStyle(
-                              fontSize: 22,
-                              color: Colors.white.withOpacity(0.53),
-                              fontWeight: FontWeight.bold,
-                              fontFamily: const String.fromEnvironment('InriaSans'),
-                            ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: BlocBuilder<HealthTestBloc, HealthTestState>(
+                    builder: (context, healthTestState) {
+                      if (healthTestState is HealthTestLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (healthTestState is HealthTestLoaded) {
+                        return SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const SizedBox(height: 40),
+                              _buildHealthTestsList(healthTestState.healthTests, screenWidth, role),
+                            ],
                           ),
-                        ),
-                      ),
-                      Expanded(
-                        child: BlocBuilder<HealthTestBloc, HealthTestState>(
-                          builder: (context, healthTestState) {
-                            if (healthTestState is HealthTestLoading) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            } else if (healthTestState is HealthTestLoaded) {
-                              return SingleChildScrollView(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const SizedBox(height: 40),
-                                    _buildHealthTestsList(healthTestState.healthTests, screenWidth, role),
-                                  ],
-                                ),
-                              );
-                            } else if (healthTestState is HealthTestError) {
-                              return Center(
-                                child: Text(
-                                  healthTestState.message,
-                                  style: const TextStyle(color: Colors.red, fontSize: 16),
-                                ),
-                              );
-                            } else {
-                              return const Center(
-                                child: Text('Error'),
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                    ],
+                        );
+                      } else if (healthTestState is HealthTestError) {
+                        return Center(
+                          child: Text(
+                            healthTestState.message,
+                            style: const TextStyle(color: Colors.red, fontSize: 16),
+                          ),
+                        );
+                      } else {
+                        return const Center(
+                          child: Text('Error'),
+                        );
+                      }
+                    },
                   ),
-                  const Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: FooterWidget(),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        }
-      },
+            const Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: FooterWidget(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -140,69 +120,31 @@ class _PetMedicalHistorialPageState extends State<PetMedicalHistorialPage> {
         bool isSelected = _selectedIndex == index;
         double scale = isSelected ? 0.9 : 1.0;
 
-        // "New Health Test"
         if (index == 0 && role == 'vet') {
-          return GestureDetector(
+          return CustomCard(
+            screenWidth: screenWidth,
+            isSelected: isSelected,
+            scale: scale,
+            child: _buildNewHealthTestContainer(screenWidth),
             onTap: () {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            child: AnimatedScale(
-              duration: const Duration(milliseconds: 200),
-              scale: scale,
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Colors.white.withOpacity(0.53),
-                  boxShadow: isSelected
-                      ? [
-                          const BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 10,
-                            spreadRadius: 1,
-                          ),
-                        ]
-                      : [],
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CreateHealthView(petId: widget.petId, petName: widget.petName),
                 ),
-                padding: const EdgeInsets.all(8.0),
-                child: _buildNewHealthTestContainer(screenWidth),
-              ),
-            ),
+              );
+            },
           );
         }
 
         if (index > 0 && index <= healthTests.length) {
           final healthTest = healthTests[index - 1];
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            child: AnimatedScale(
-              duration: const Duration(milliseconds: 200),
-              scale: scale,
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Colors.white.withOpacity(0.53),
-                  boxShadow: isSelected
-                      ? [
-                          const BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 10,
-                            spreadRadius: 1,
-                          ),
-                        ]
-                      : [],
-                ),
-                padding: const EdgeInsets.all(16.0),
-                child: _buildHealthTestContainer(healthTest, screenWidth),
-              ),
-            ),
+          return CustomCard(
+            screenWidth: screenWidth,
+            isSelected: isSelected,
+            scale: scale,
+            child: _buildHealthTestContainer(healthTest, screenWidth),
+            onTap: () => setState(() => _selectedIndex = index),
           );
         }
 
@@ -212,92 +154,54 @@ class _PetMedicalHistorialPageState extends State<PetMedicalHistorialPage> {
   }
 
   Widget _buildNewHealthTestContainer(double screenWidth) {
-    return GestureDetector(
-      onTap: _onNewHealthTestPressed,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: screenWidth * 0.1,
-            height: screenWidth * 0.1,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-            ),
-            child: const Icon(
-              Icons.add,
-              color: Color(0xFF4B8DAF),
-              size: 50.0,
-            ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: screenWidth * 0.1,
+          height: screenWidth * 0.1,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
           ),
-          const SizedBox(width: 10),
-          Text(
-            'New Health Test',
-            style: TextStyle(
-              color: const Color(0xFF4B8DAF),
-              fontSize: screenWidth * 0.06,
-              fontWeight: FontWeight.bold,
-            ),
+          child: const Icon(
+            Icons.add,
+            color: Color(0xFF4B8DAF),
+            size: 50.0,
           ),
-        ],
-      ),
-    );
-  }
-
-  void _onNewHealthTestPressed() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CreateHealthView(petId: widget.petId, petName: widget.petName),
-      ),
+        ),
+        const SizedBox(width: 10),
+        Text(
+          'New Health Test',
+          style: TextStyle(
+            color: const Color(0xFF4B8DAF),
+            fontSize: screenWidth * 0.06,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 
   Widget _buildHealthTestContainer(HealthTest healthTest, double screenWidth) {
     return ListTile(
-      title: Row(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '• TYPE: ${healthTest.testName}',
-                  style: TextStyle(
-                    color: const Color(0xFF065591),
-                    fontSize: screenWidth * 0.035,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '• DESCRIPTION: ${healthTest.description}',
-                  style: TextStyle(
-                    color: const Color(0xFF065591),
-                    fontSize: screenWidth * 0.035,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '• DATE: ${DateFormat.yMMMd().format(healthTest.date)}',
-                  style: TextStyle(
-                    color: const Color(0xFF065591),
-                    fontSize: screenWidth * 0.035,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '• PLACE: ${healthTest.place}',
-                  style: TextStyle(
-                    color: const Color(0xFF065591),
-                    fontSize: screenWidth * 0.035,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          _buildHealthTestDetail('TYPE', healthTest.testName, screenWidth),
+          _buildHealthTestDetail('DESCRIPTION', healthTest.description, screenWidth),
+          _buildHealthTestDetail('DATE', DateFormat.yMMMd().format(healthTest.date), screenWidth),
+          _buildHealthTestDetail('PLACE', healthTest.place, screenWidth),
         ],
       ),
+    );
+  }
+
+  Widget _buildHealthTestDetail(String label, String value, double screenWidth) {
+    return CustomText(
+      text: '• $label: $value',
+      fontSize: screenWidth * 0.035,
+      fontWeight: FontWeight.bold,
+      color: const Color(0xFF065591),
     );
   }
 }
